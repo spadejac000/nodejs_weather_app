25d9fee47fa6b5c0e0646bdc8a91e791a3a9fa45
Jacob Spade <spade.jacob@Yahoo.com>
2019-01-27 23:22:25 -0800

    targeted data from api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-let express = require('express');
+const express = require('express');
+const request = require('request');
 
-let app = express();
+const app = express();
 
 app.set('view engine', 'ejs');
 
@@ -9,7 +10,22 @@ let api = '26318328797296678be8b407dcc293ad';
 let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api}`;
 
 app.get('/', (req, res) => {
-  res.render('weather');
+  request(url, (error, response, body) => {
+    
+    let weather_json = JSON.parse(body);
+    console.log(weather_json.list);
+
+    // let weather = {
+    //   city,
+    //   temperature: Math.round(weather_json.main.temperature),
+    //   description: weather_json.weather[0].despcription,
+    //   icon: weather_json.weather[0].icon,
+    // };
+
+    // let weather_data = {weather: weather}
+
+    // res.render('weather', weather_data);
+  });
 });
 
 app.listen(8000);
