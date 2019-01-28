ec020339b3b95894498cb5693bb14bd54b7b692a
Jacob Spade <spade.jacob@Yahoo.com>
2019-01-28 00:34:05 -0800

    rendered weather to front end

--- server.js
+++ server.js
@@ -7,24 +7,24 @@ app.set('view engine', 'ejs');
 
 let city = 'Seattle';
 let api = '26318328797296678be8b407dcc293ad';
-let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api}`;
+let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api}&units=imperial`;
 
 app.get('/', (req, res) => {
   request(url, (error, response, body) => {
     
     let weather_json = JSON.parse(body);
-    console.log(weather_json.list);
+    console.log(weather_json.list[0]);
 
-    // let weather = {
-    //   city,
-    //   temperature: Math.round(weather_json.main.temperature),
-    //   description: weather_json.weather[0].despcription,
-    //   icon: weather_json.weather[0].icon,
-    // };
+    let weather = {
+      city,
+      temperature: Math.round(weather_json.list[0].main.temp),
+      description: weather_json.list[0].weather[0].description,
+      icon: weather_json.list[0].weather[0].icon,
+    };
 
-    // let weather_data = {weather: weather}
+    let weather_data = {weather: weather}
 
-    // res.render('weather', weather_data);
+    res.render('weather', weather_data);
   });
 });
 
